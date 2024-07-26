66f5814b7dc020272dd1a390f609c0a52853101a
AIS <AIS@DESKTOP-7I1DFBE>
2024-07-26 14:28:31 +0530

    Add project files.

---
+++ Hostel_Management/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hostel_Management.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [Route("Hotel")]
+        public IActionResult Hotel()
+        {
+            return View();
+        }
+        [Route("Schedule_Meeting")]
+        public IActionResult Meeting()
+        {
+            return View();
+        }
+    }
+}
---
+++ Hostel_Management/wwwroot/js/site.js
@@ -0,0 +1,10 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+$(document).ready(function () {
+    $('.chat_icon').click(function (event) {
+        $('.chat-box').toggleClass('active');
+    });
+    $('.conv-form-wrapper').convform({ selectInputStyle: 'disable' });
+})
